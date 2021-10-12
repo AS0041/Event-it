a3415b439e5ca8869f957ee43b0b43040da51d83
Abhishek Singh <79040210+AS0041@users.noreply.github.com>
2021-10-12 15:20:29 +0530

    l

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -43,18 +43,3 @@ function createOneElement() {
     // Finally put it where it is supposed to appear.
     document.getElementById("newelement").appendChild(txtNewInputBox);
 }
-var x = setInterval(function () {
-    var value = new Date(document.getElementById("value").value).getTime();
-    var presentTime = new Date().getTime();
-    var distance = value - presentTime;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerText = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "<h3 class='is-size-2 live'>Event is Live Now!</h3>";
-    }
-}, 1000);
